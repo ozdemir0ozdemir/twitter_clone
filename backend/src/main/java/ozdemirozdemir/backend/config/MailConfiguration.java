8f248e8cf93f0b3f132fd52198086c7456a08634
Özdemir Özdemir <ozdemirozdemir@hotmail.com.tr>
2024-07-23 17:25:12 +0300

    Episode 9 Finished

---
+++ backend/src/main/java/ozdemirozdemir/backend/config/MailConfiguration.java
@@ -0,0 +1,58 @@
+package ozdemirozdemir.backend.config;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.GmailScopes;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.*;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class MailConfiguration {
+
+    private static final String APPLICATION_NAME = "Fwitter";
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Collections.singletonList(GmailScopes.GMAIL_SEND);
+    private static final String CREDENTIALS_FILE = "/credentials.json";
+
+    private Credential getCredentials(final NetHttpTransport httpTransport) throws IOException {
+
+        InputStream in = MailConfiguration.class.getResourceAsStream(CREDENTIALS_FILE);
+
+        if (in == null) {
+            throw new FileNotFoundException("Credentials file not found!");
+        }
+
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow
+                .Builder(
+                    httpTransport,
+                    JSON_FACTORY,
+                    clientSecrets,
+                    SCOPES
+                )
+                .setDataStoreFactory(new FileDataStoreFactory(new File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder()
+                .setPort(8888)
+                .build();
+
+        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+
+        return credential;
+    }
+
+
+}
