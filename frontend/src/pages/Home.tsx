a702df284294c25e0bb943fec4f1ab64d38b13a3
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-07-26 22:02:48 +0300

    Episode 14 Finished

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import './Home.css';
+import '../assets/global.css';
+
+export const Home:React.FC = () => {
+    return (
+        <div className="home-container bg-color">
+            <h1>Home</h1>
+        </div>
+    )
+}
