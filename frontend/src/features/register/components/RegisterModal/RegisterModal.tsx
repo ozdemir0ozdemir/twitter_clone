27c0ad87d43db9714aa602cecd59cb63aacba1dd
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-07-26 22:27:35 +0300

    Episode 15 Finished

---
+++ frontend/src/features/register/components/RegisterModal/RegisterModal.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import {Modal} from "../../../../components/Modal/Modal";
+
+import './RegisterModal.css';
+
+export const RegisterModal:React.FC = () => {
+    return (
+        <div className="register-modal">
+            <Modal >
+                <h1>Register Modal</h1>
+            </Modal>
+        </div>
+    )
+}
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import './Home.css';
 import '../assets/global.css';
+import {RegisterModal} from "../features/register/components/RegisterModal/RegisterModal";
 
 export const Home:React.FC = () => {
     return (
         <div className="home-container bg-color">
-            <h1>Home</h1>
+            <RegisterModal />
+
         </div>
     )
 }
