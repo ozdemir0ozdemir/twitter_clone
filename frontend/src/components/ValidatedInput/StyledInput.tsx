9577f39cb747a8eaacd2ed4b9bd35a8d9abab7c3
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-08-05 12:25:35 +0300

    Episode 21 Finished

--- frontend/src/components/ValidatedInput/StyledInput.tsx
+++ frontend/src/components/ValidatedInput/StyledInput.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import {StyledInputProps} from '../../utils/GlobalInterfaces';
-import {determineStyledInputBorder} from "../../utils/DetermineStylesUtil";
+import {determineLabelColor, determineStyledInputBorder} from "../../utils/DetermineStylesUtil";
 
 
 export const StyledInputBox = styled.div<StyledInputProps>`
@@ -10,4 +10,16 @@ export const StyledInputBox = styled.div<StyledInputProps>`
     width: 100%;
     height: 56px;
     border: ${(props => determineStyledInputBorder(props))}
+`;
+
+export const StyledInputLabel = styled.span<StyledInputProps>`
+    position: absolute;
+    left: 10px;
+    font-weight: 400;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    font-size: ${props => props.active ? '13px' : '18px'};
+    top: ${props => props.active ? '5px' : '16px'};
+    color: ${props => determineLabelColor(props)};
 `;
